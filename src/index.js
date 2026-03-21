88a090bbea2cd7968943d9aa87a1d6f6449a2758
Tushar Raj <tr6056538@gmail.com>
2026-03-22 03:58:26 +0530

    chnaged code of index.js to fix package error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import { scanDOM } from "./core/scanner.js";
-import { applyStyles } from "./core/styler.js";
-
-export default function chaiTailwind() {
+function chaiTailwind() {
 
   const elements = scanDOM();
 
   applyStyles(elements);
-
+  
 }
+
+export default chaiTailwind;
 
 if (typeof window !== "undefined") {
 
   window.chaiTailwind = chaiTailwind;
-  
+
 }
